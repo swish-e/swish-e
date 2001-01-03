ccaf238ca213009917710d4406aff4f54b45a7ef
jmruiz <jmruiz@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2001-01-03 13:15:49 +0000

    Updated swishe.xs and test.pl to add datetime support to SwishNext

--- swish-e/perl/test.pl
+++ swish-e/perl/test.pl
@@ -1,164 +1,113 @@
-#!/usr/local/bin/perl -w
+#!/usr/local/bin/perl
 
-    use strict;
+use SWISHE;
 
-=pod
-    Test script for the SWISHE library
-    please see perldoc README-PERL for more information
-    $Id$
-=cut
+#$properties='prop1 prop2 prop3';
+#$sortspec='prop1 asc prop2 desc';
+$searchstring='meta1=metatest1';
 
-    # Import symbols
-    use SWISHE;
+$indexfilename1='../tests/test.index';
+$indexfilename2='../tests/test.index';
+#$indexfilename2='another.index';
+$indexfilename="$indexfilename1";
+# To search for several index just put them together
+$indexfilename="$indexfilename1 $indexfilename2";
 
-
-    # In this test we will use the same index twice
-    
-    my $indexfilename1 = '../tests/test.index';
-    my $indexfilename2 = $indexfilename1;
-
-    die "Index file '$indexfilename1' not found!  Did you run make test?\n"
-        unless -e $indexfilename1;
-
-    my $indexfiles = $indexfilename1;        
+#Stemming sample
+$word="parking";
+$stem_word=SwishStem($word);
+print "$word is stemmed as $stem_word\n";
 
 
-    # To search for several indexes just put them together
-    #my $indexfiles = "$indexfilename1 $indexfilename2";
-
-
-    # Open the index files
-    
-    my $handle = SwishOpen( $indexfiles )
-        or die "Failed to open '$indexfiles'";
-
-
-    # Get a few headers from the index files
-
-    my @headers = qw/WordCharacters BeginCharacters EndCharacters/;
-    push @headers, 'Indexed on';
-  
-    for ( @headers ) {
-        print_header("Header '$_'");
-
-        my @h =  SwishHeaderParameter( $handle, $_ );
-        print "$_ for index 0 is $h[0]\n";
-    }
-
-
-    # Define a few searches
-
-    my @searches = (
-        {
-            title   => 'Normal search',
-            query   => 'test',
-            props   => '',
-            sort    => '',
-            context => 1,   # Search the entire file
-        },
-        {
-            title   => 'MetaTag search 1',
-            query   => 'meta1=metatest1',
-            props   => 'meta1 meta2 meta3',
-            sort    => '',
-            context => 1,   # Search the entire file
-        },
-        {
-            title   => 'MetaTag search 2',
-            query   => 'meta2=metatest2',
-            props   => 'meta1 meta2 meta3',
-            sort    => '',
-            context => 1,   # Search the entire file
-        },
-        {
-            title   => 'XML Search',
-            query   => 'meta3=metatest3',
-            props   => 'meta1 meta2 meta3',
-            sort    => '',
-            context => 1,   # Search the entire file
-        },
-        {
-            title   => 'Phrase Search',
-            query   => 'meta3="three little pigs"',
-            props   => 'meta1 meta2 meta3',
-            sort    => '',
-            context => 1,   # Search the entire file
-        },
-        {
-            title   => 'Advanced search',
-            query   => 'test or meta1=m* or meta2=m* or meta3=m*',
-            props   => 'meta1 meta2 meta3',
-            sort    => '',
-            context => 1,   # Search the entire file
-        },
-    );
-
-    my @settings = qw/query context props sort/;
-
-    print_header("*** Now searching ****");
-    print "Note that some META names have embedded newlines.\n";
-
-
-    for my $search ( @searches ) {
-        print_header( "$search->{title} - Query: '$search->{query}'" );
-              
-        my $num_results = SwishSearch( $handle, @{$search}{ @settings } );
-
-        print "# Number of results = $num_results\n\n";
-
-        unless ( $num_results ) {
-            print "No Results\n";
-
-            my $error = SwishError( $handle );
-            print "Error number: $error\n" if $error;
-
-            next;
-        }
-
-        while( my($rank,$index,$file,$title,$summary,$start,$size,@props) = SwishNext( $handle )) {
-            print join( ' ',
-                  $rank,
-                  $index,
-                  $file,
-                  qq["$title"],
-                  qq["$summary"],
-                  $start,
-                  $size,
-                  map{ qq["$_"] } @props,
-                  ),"\n";
-        }
-    }
-
-    print_header('Other Functions');
-
-
-
-    my @stemwords = qw/parking libaries library librarians money monies running runs is/;
-    print "\nStemming:\n";
-    print "    '$_' => '" . SwishStem( $_ ) . "'\n" for @stemwords;
-    print "\n";
-
-    my @stopwords = SwishStopWords( $handle, $indexfilename1 );
-    print 'Stopwords: ',
-          ( @stopwords ? join(', ', @stopwords) : '** None **' ),
-          "\n\n";
-
-
-    my $letter = 't';
-    my @keywords = SwishWords( $handle, $indexfilename1, $letter);
-
-    print "List of keywords that start with the letter '$letter':\n",
-          join("\n", @keywords),
-          "\n\n";
-
-
-
-    SwishClose( $handle );
-
-sub print_header {
-    print "\n", '-' x length( $_[0] ),"\n",
-          $_[0],
-          "\n", '-' x length( $_[0] ),"\n";
+unless($handle=SWISHE::SwishOpen($indexfilename))
+{
+	print "Could not open index files\n";
+	die;
 }
 
+# Need some info from header ? Here is how
+# Since we have open two files, two values are returned
+@wordchars=SWISHE::SwishHeaderParameter($handle,"WordCharacters");
+print "WordCharacters 0 = @wordchars[0]\n";
+print "WordCharacters 1 = @wordchars[1]\n";
 
+@beginchars=SWISHE::SwishHeaderParameter($handle,"BeginCharacters");
+print "BeginCharacters 0 = @beginchars[0]\n";
+print "BeginCharacters 1 = @beginchars[1]\n";
+
+@endchars=SWISHE::SwishHeaderParameter($handle,"EndCharacters");
+print "EndCharacters 0 = @endchars[0]\n";
+print "EndCharacters 1 = @endchars[1]\n";
+
+@ignorefirstchar=SWISHE::SwishHeaderParameter($handle,"IgnoreFirstChar");
+print "IgnoreFirstChar 0 = @ignorefirstchar[0]\n";
+print "IgnoreFirstChar 1 = @ignorefirstchar[1]\n";
+
+@ignorelastchar=SWISHE::SwishHeaderParameter($handle,"IgnoreLastChar");
+print "IgnoreLastChar 0 = @ignorelastchar[0]\n";
+print "IgnoreLastChar 1 = @ignorelastchar[1]\n";
+
+@indexedon=SWISHE::SwishHeaderParameter($handle,"Indexed on");
+print "Indexed on 0 = @indexedon[0]\n";
+print "Indexed on 1 = @indexedon[1]\n";
+
+@description=SWISHE::SwishHeaderParameter($handle,"Description");
+print "Description 0 = @description[0]\n";
+print "Description 1 = @description[1]\n";
+
+@indexpointer=SWISHE::SwishHeaderParameter($handle,"IndexPointer");
+print "IndexPointer 0 = @indexpointer[0]\n";
+print "IndexPointer 1 = @indexpointer[1]\n";
+
+@indexadmin=SWISHE::SwishHeaderParameter($handle,"IndexAdmin");
+print "IndexAdmin 0 = @indexadmin[0]\n";
+print "IndexAdmin 1 = @indexadmin[1]\n";
+
+@stemming=SWISHE::SwishHeaderParameter($handle,"Stemming");
+print "Stemming 0 = @stemming[0]\n";
+print "Stemming 1 = @stemming[1]\n";
+
+@soundex=SWISHE::SwishHeaderParameter($handle,"Soundex");
+print "Soundex 0 = @soundex[0]\n";
+print "Soundex 1 = @soundex[1]\n";
+
+# Do you want know the stopwords? Here is how
+@stopwords=SWISHE::SwishStopWords($handle,$indexfilename1);
+print "StopWords =";
+for($i=0;@stopwords[$i];$i++)
+{
+	print " @stopwords[$i]"
+}
+print "\n";
+
+# Do you want know the indexeded words starting with 't'? Here is how
+@keywords=SWISHE::SwishWords($handle,$indexfilename1,"t");
+print "KeyWords =";
+for($i=0;@keywords[$i];$i++)
+{
+	print " @keywords[$i]"
+}
+print "\n";
+
+$structure=1;
+
+# Uncomment for an endless loop
+#while (<>)
+#{
+$num_results=SwishSearch($handle,$searchstring,$structure,$properties,$sortspec);
+
+if ($num_results<0) 
+{
+	print "Search error: $num_results\n";
+} else{ 
+	print "Search Results: $num_results\n";
+}
+
+while(($rank,$indexfile,$filename,$docdate,$title,$summary,$start,$size,$prop1,$prop2,$prop3)=SWISHE::SwishNext($handle))
+{
+   print "$rank $indexfile $filename \"$docdate\" \"$title\" \"$summary\" $start $size \"$prop1\" \"$prop2\" \"$prop3\"\n";
+}
+# Uncomment for an endless loop
+#}
+
+SWISHE::SwishClose($handle);
