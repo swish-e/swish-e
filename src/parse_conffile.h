57cd994f79248d635b5b84e96b187f9e26131b71
rasc <rasc@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2001-03-05 21:06:35 +0000

    doctype check in own routine (code optimize)

--- src/parse_conffile.h
+++ src/parse_conffile.h
@@ -1,7 +1,8 @@
 
 void grabCmdOptions(StringList *sl, int start, struct swline **listOfWords);
 void getdefaults(SWISH *sw, char *conffile, int *hasdir, int *hasindex, int hasverbose);
-int getYesNoOrAbort (StringList *sl, int n, int last);
+int getYesNoOrAbort (StringList *sl, int n, int islast);
+int getDocTypeOrAbort (StringList *sl, int n);
 
 void readstopwordsfile(SWISH *, IndexFILE *, char *);
 void readusewordsfile(SWISH *, IndexFILE *, char *);
