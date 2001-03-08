e483e87619c66cd762bc9c7ca546b22b7e848142
whmoseley <whmoseley@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2001-03-08 18:40:50 +0000

    Tried to fix a few warnings, but still need some help:
    There are a number of these:
    
    lst.c:31: warning: `begintag' might be used uninitialized in this function
    lst.c:31: warning: `endtag' might be used uninitialized in this function
    lst.c:33: warning: `lentag' might be used uninitialized in this function
    
    But from what I could find that maybe that's just an issuse with my gcc
    version.
    
    > gcc --version
    egcs-2.91.66
    
    I saw reports where 2.95 cleared up all those "might be used uninitialized"
    errors.
    
    And these were reported on the swish-e about a year ago:
    
    docprop.h:29: warning: `struct metaMergeEntry' declared inside parameter list
    docprop.h:29: warning: its scope is only this definition or declaration,
    docprop.h:29: warning: which is probably not what you want.
    
    So any help with those would be great

--- swish-e/src/result_output.c
+++ swish-e/src/result_output.c
@@ -1,5 +1,5 @@
 /*
-$id:$
+$id$
 ** Copyright (C) 1995, 1996, 1997, 1998 Hewlett-Packard Company
 ** Originally by Kevin Hughes, kev@kevcom.com, 3/11/94
 **
--- swish-e/src/result_sort.h
+++ swish-e/src/result_sort.h
@@ -5,6 +5,8 @@ char **getResultSortProperties (SWISH *, IndexFILE *, docPropertyEntry *);
 
 int initSortResultProperties (SWISH *);
 
+void sortFileProperties(IndexFILE *indexf);
+
 RESULT *addsortresult(SWISH *, RESULT *sp, RESULT *);
 RESULT *sortresults (SWISH *, int );
 
