fa88aaf3f313de48fb9c1e6980dd1e6b3fc1c2ad
augur <augur@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2001-04-21 08:16:41 +0000

    Collect various Win32-specific definitions scattered about the source tree into win32/config.h.  Many of the Win32-specific defines can now be safely removed from the rest of the source.  Any new Win32 definitions should be placed here where possible.

--- src/win32/config.h
+++ src/win32/config.h
@@ -1,8 +1,33 @@
 #ifdef _WIN32
-#define SWISH_VERSION "2.1-dev-20" 
-#define NO_GETTOD
+#include "win32/dirent.h"
+#include "win32/regex.h"
 
-#define popen _popen
-#define pclose _pclose
+#include <stdlib.h>			/* _sleep() */
+#include <process.h>			/* _getpid() */
+
+/* ifdef logic  */
+#define NO_GETTOD			/* Win32 has no Get Time Of Day  */
+#define NO_SYMBOLIC_FILE_LINKS		/* Win32 has no symbolic links */
+#undef INDEXPERMS			/* Win32 has no chmod() */
+
+/* Macros which rewrite values  */
+#define SWISH_VERSION "2.1-dev-20"	/* Should we find a better way to handle this */
+#define TMPDIR "c:\\windows\\temp"	/* Should this be taken from the environment at runtime */
+#define DIRDELIMITER '\\'		/* Does this work right?  Might explain dir problems which have reported */
+
+#define FILEMODE_READ		"rb"	/* Read only */
+#define FILEMODE_WRITE		"wb"	/* Write only */
+#define FILEMODE_READWRITE	"rb+"	/* Read Write */
+
+/* Type definitions */
+typedef int pid_t;			/* process ID */
+
+/* Rewrite ANSI functions to Win32 equivalents */
+#define popen		_popen
+#define pclose		_pclose
+#define strcasecmp	stricmp
+#define strncasecmp	strnicmp
+#define sleep		_sleep
+#define getpid		_getpid
 
 #endif
