6112564ddf6f5aa05936b34965034e7a64f72480
whmoseley <whmoseley@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2001-06-09 16:59:12 +0000

    Made adjustments as suggested by Bill.  Thanks very much for looking.
    Now, how 'bout that binary search?
    
    Finding an inclusive range is such a common thing
    I'm sure there must be an easier way.  I think the
    code is doing too much work currently.
    
    I also wonder if I need to be passing so many variables --
    If I pass IndexFILE do I need to pass sw?

--- src/proplimit.h
+++ src/proplimit.h
@@ -1,8 +1,11 @@
+#ifndef __HasSeenModule_PropLimit
+#define __HasSeenModule_PropLimit  1
 void initModule_PropLimit (SWISH  *sw);
 void freeModule_PropLimit (SWISH *sw);
 int Prepare_PropLookup(SWISH *sw );
 void SetLimitParameter(SWISH *sw, char *propertyname, char *low, char *hi);
 int LimitByProperty( IndexFILE *indexf, int filenum );
+#endif
 
 
 
