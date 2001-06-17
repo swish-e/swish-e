b6959719da99d9eaffdc6f3cff4300fe1d3843c6
meier <meier@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2001-06-17 19:34:02 +0000

    Add improved memory routines. See mem.h for settings

--- src/mem.c
+++ src/mem.c
@@ -1,6 +1,5 @@
 /*
-** Copyright (C) 1995, 1996, 1997, 1998 Hewlett-Packard Company
-** Originally by Kevin Hughes, kev@kevcom.com, 3/11/94
+$Id$
 **
 ** This program and library is free software; you can redistribute it and/or
 ** modify it under the terms of the GNU (Library) General Public License
@@ -16,114 +15,420 @@
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 **
-**  2001-02-15   rasc   estrredup, type corrections
+** Author: Bill Meier, June 2001
 **
+** To Do:
+**     Memory statistics doesn't really require use of Mem_ routines
 */
 
-#include "swish.h"
-#include "mem.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
 #include "error.h"
 
-/* Error-checking malloc()...
-*/
-
-#ifdef DEBUGMEMORY
-
-struct memory {
-	void *p;
-	int sz;
-};
-
-#define MAXMEMARRAY 1000000
-int firsttime=1;
-struct memory mem[MAXMEMARRAY];
-
-void *emalloc(size_t i)
-{
-int j;
-void *p;
-if (firsttime) { firsttime = 0; for(j=0;j<MAXMEMARRAY;j++) mem[j].p=NULL; }
-
-for(j=0;j<MAXMEMARRAY;j++) if (!mem[j].p) break;
-if (j==MAXMEMARRAY) progerr("Out memory in Debug Array");	 
-
-        if ((p = (void *) malloc(i)) == NULL)
-                progerr("Ran out of memory (could not allocate enough)!");
-mem[j].p = (void *)p;
-mem[j].sz = i;
-        return p;
-}
-
-void *erealloc(void *ptr, size_t i)
-{
-void *p;
-int j;
-if (firsttime) progerr("Memory error. Calling erealloc without emalloc");
-for(j=0;j<MAXMEMARRAY;j++) if (mem[j].p == ptr) break;
-if (j==MAXMEMARRAY) progerr("Error reallocating memory. Original pointer not found");	 
-        if ((p = (void *) realloc(ptr, i)) == NULL)
-                progerr("Ran out of memory (could not reallocate enough)!");
-mem[j].p = (void *)p;
-mem[j].sz = i;
-        return p;
-}
+#include "mem.h"
 
 
-void efree(void *ptr)
-{
-int j;
-if (firsttime) progerr("Memory error. Calling efree without emalloc or estrdup");
-for(j=0;j<MAXMEMARRAY;j++) if (mem[j].p == ptr) break;
-if (j==MAXMEMARRAY) progerr("Error freeing memory. Pointer not found");
-        free(ptr);
-mem[j].p = NULL;
-}
+/* simple cases first ... */
 
-void checkmem(void)
-{
-int j,k,l;
-char *p;
-printf("Unallocated Memory:\n");
-for(j=0;j<MAXMEMARRAY;j++) 
-	if (mem[j].p) {
-		printf("Pointer: %p Size: %d Contents:",mem[j].p,mem[j].sz);
-		if(mem[j].sz>50)l=50;
-		else l=mem[j].sz;
-		for(k=0,p=mem[j].p;k<l;k++,p++) if(isalnum((int)((unsigned char)p[0])) || isspace((int)((unsigned char)p[0]))) putchar((int)p[0]);
-		putchar('\n');
-	}
-}
+#if ! (MEM_DEBUG | MEM_TRACE | MEM_STATISTICS)
 
-#else
 
-void checkmem(void)
-{
-	return;
-}
-
-void *emalloc(size_t i)
+/* emalloc - malloc a block of memory */
+void *emalloc(size_t size)
 {
 	void *p;
-	
-	if ((p = (void *) malloc(i)) == NULL)
+
+	if ((p = malloc(size)) == NULL)
 		progerr("Ran out of memory (could not allocate enough)!");
+
 	return p;
 }
 
-void *erealloc(void *ptr, size_t i)
+
+/* erealloc - realloc a block of memory */
+void *erealloc(void *ptr, size_t size)
 {
 	void *p;
 	
-	if ((p = (void *) realloc(ptr, i)) == NULL)
+	if ((p = realloc(ptr, size)) == NULL)
 		progerr("Ran out of memory (could not reallocate enough)!");
+
 	return p;
 }
 
 
+/* efree - free a block of memory */
 void efree(void *ptr)
 {
 	free(ptr);
 }
 
+
+/* Mem_Summary - print out a memory usage summary */
+void Mem_Summary(char *title, int final)
+{
+	return;
+}
+
+
+
+#else	// (MEM_DEBUG | MEM_TRACE | MEM_STATISTICS)
+
+
+
+/*
+** Now we get into the debugging/trace memory allocator. I apologize for the
+** conditionals in this code, but it does work :-)
+*/
+
+/* Random value for a GUARD at beginning and end of allocated memory */
+#define GUARD 0x14739182
+
+#if MEM_TRACE
+
+typedef struct
+{
+	char	*File;
+	int		Line;
+//	void	*Ptr;	// only needed for extra special debugging
+	size_t	Size;
+} TraceBlock;
+
 #endif
 
+
+/* MemHeader is what is before the user allocated block - for our bookkeeping */
+typedef struct 
+{
+#if MEM_TRACE
+	TraceBlock		*Trace;
+#endif
+#if MEM_DEBUG
+    unsigned long	Guard1;
+#endif
+    size_t	Size;
+#if MEM_DEBUG
+    void			*Start;
+    unsigned long	Guard2;
+#endif
+} MemHeader;
+
+
+/* MemTail is what is after the user allocated block - for our bookkeeping */
+#if MEM_DEBUG
+typedef struct
+{
+    unsigned long	Guard;
+} MemTail;
+
+
+/* Define the extra amount of memory we need for our bookkeeping */
+#define MEM_OVERHEAD_SIZE (sizeof(MemHeader) + sizeof(MemTail))
+#else
+#define MEM_OVERHEAD_SIZE (sizeof(MemHeader))
+#endif
+
+
+#if MEM_TRACE
+
+#define MAX_TRACE 1000000
+
+static TraceBlock	TraceData[MAX_TRACE];
+static TraceBlock	*Free = NULL;
+static int			last;
+
+#endif
+
+static unsigned long MAllocCalls = 0;
+static unsigned long MReallocCalls = 0;
+static unsigned long MFreeCalls = 0;
+static unsigned long MAllocCurrentOverhead = 0;
+static unsigned long MAllocMaximumOverhead = 0;
+static unsigned long MAllocCurrent = 0;
+static unsigned long MAllocMaximum = 0;
+
+
+#if MEM_TRACE
+
+static TraceBlock *AllocTrace(char *file, int line, void *ptr, size_t size)
+{
+	TraceBlock	*Block;
+	int			i;
+
+	if (Free)
+	{
+		Block = Free;
+		Free = NULL;
+	}
+	else
+	{
+		for (i = last; i < MAX_TRACE; i++)	/** $$$ What if we run off the end? **/
+			if (TraceData[i].File == NULL)
+				break;
+
+		Block = &TraceData[i];
+		last = i + 1;
+	}
+
+	Block->File = file;
+	Block->Line = line;
+//	Block->Ptr = ptr;		// only needed for extra special debugging
+	Block->Size = size;
+
+	return Block;
+}
+
+#endif
+
+
+/* Mem_Alloc - Allocate a chunk of memory */
+
+void * Mem_Alloc (size_t Size, char *file, int line)
+
+/*
+ * FUNCTIONAL DESCRIPTION:
+ *
+ *	This routine will allocate a chunk of memory of a specified size.
+ *
+ * FORMAL PARAMETERS:
+ *
+ *	Size		Size in bytes of the chunk to allocate
+ *
+ * ROUTINE VALUE:
+ *
+ *	Address of allocated memory
+ *
+ * SIDE EFFECTS:
+ *
+ *	Severe error is signaled if can't allocate memory
+ */
+
+    {
+
+    size_t		MemSize;		/* Actual size of memory to alloc */
+    unsigned char *MemPtr;		/* Pointer to allocated memory */
+    MemHeader	*Header; 		/* Header of memory */
+
+
+    /*
+     *  Adjust size to account for the memory header and tail information we
+     *  include. This is so we know how much we allocated to be able to free it,
+     *  and also in support of memory debugging routines.
+     */
+
+    MemSize = Size + MEM_OVERHEAD_SIZE;
+
+    /*
+     *  Get the memory; die if we can't...
+     */
+
+    MemPtr = (unsigned char *)malloc(MemSize);
+
+	if (MemPtr == NULL)
+		progerr("Ran out of memory (could not allocate enough)!");
+
+
+    /*
+     *  Keep a running total of the memory allocated for statistical purposes.
+     *  Save the chunk size in the first long word of the chunk, and return the
+     *  address of the chunk (following the chunk size) to the caller.
+     */
+
+    MAllocCalls++;
+    MAllocCurrentOverhead += MEM_OVERHEAD_SIZE;
+
+    if (MAllocCurrentOverhead > MAllocMaximumOverhead)
+		MAllocMaximumOverhead = MAllocCurrentOverhead;
+
+    MAllocCurrent += Size;
+
+    if (MAllocCurrent > MAllocMaximum)
+		MAllocMaximum = MAllocCurrent;
+
+    Header = (MemHeader *)MemPtr;
+    Header->Size = Size;
+    MemPtr = MemPtr + sizeof (MemHeader);
+
+    /*
+     * Add guards, and fill the memory with a pattern
+     */
+
+#if MEM_DEBUG 
+    {
+    MemTail *Tail;
+    
+    Header->Start = MemPtr;
+    Header->Guard1 = GUARD;
+    Header->Guard2 = GUARD;
+    memset(MemPtr, 0xAA, Size);
+    Tail = (MemTail *)(MemPtr + Size);
+    Tail->Guard = GUARD;
+    }
+#endif
+
+#if MEM_TRACE
+	Header->Trace = AllocTrace(file, line, MemPtr, Size);
+#endif
+
+//	printf("Alloc: %s line %d: Addr: %08X Size: %d\n", file, line, MemPtr, Size);
+
+    return (MemPtr);
+}
+
+
+/* Mem_Free - Free a chunk of memory */
+
+void  Mem_Free (void *Address, char *file, int line)
+
+/*
+ * FUNCTIONAL DESCRIPTION:
+ *
+ *	This routine will free a chunk of previously allocated memory.
+ *	This memory must have been allocated via  Mem_Alloc.
+ *
+ * FORMAL PARAMETERS:
+ *
+ *	Address		Address of the memory chunk to free
+ *
+ * ROUTINE VALUE:
+ *
+ *	NONE
+ *
+ * SIDE EFFECTS:
+ *
+ *	Severe error is signaled if can't free the memory
+ */
+
+{
+
+    MemHeader	*Header;
+    size_t		MemSize;		/* Size of chunk to free */
+    size_t		UserSize;
+    void		*MemPtr;
+
+	/* ANSI allows free of NULL */
+
+	if (!Address)
+		return;
+
+    /*
+     *  Get the size of the chunk to free from the long word preceding the
+     *  address of the chunk.
+     */
+
+    Header = (MemHeader *)Address - 1;
+
+#if MEM_DEBUG
+    {
+    MemTail *Tail;
+
+    if (Address != Header->Start)
+		printf("\nAlready free: %0X\n", Address); 
+		// Err_Signal (PWRK$_BUGMEMFREE, 1, Address);
+
+    if (Header->Guard1 != GUARD)
+		printf("\nGuard 1: %0X\n", &Header->Guard1);
+		// Err_Signal (PWRK$_BUGMEMGUARD1, 4, Address,
+		//	Header->Guard1, 4, &Header->Guard1);
+
+    if (Header->Guard2 != GUARD)
+		printf("\nGuard 2: %0X\n", &Header->Guard2);
+		// Err_Signal (PWRK$_BUGMEMGUARD1, 4, Address,
+		//	Header->Guard2, 4, &Header->Guard2);
+
+    Tail = (MemTail *)((unsigned char *)Address + Header->Size);
+
+    if (Tail->Guard != GUARD)
+		printf("\nTail Guard: %0X\n", &Tail->Guard);
+		// Err_Signal (PWRK$_BUGMEMGUARD2, 4, Address,
+		//	Tail->Guard, 4, &Tail->Guard);
+
+    }
+#endif
+
+
+    MFreeCalls++;
+    MemPtr = (unsigned char *)Header;
+    UserSize = Header->Size;
+    MemSize = UserSize + MEM_OVERHEAD_SIZE;
+
+//	printf("Free: %s line %d: Addr: %08X Size: %d\n", file, line, Address, UserSize);
+
+#if MEM_TRACE
+	Free = Header->Trace;
+//	printf("  Allocated at %s line %d:\n", Free->File, Free->Line);
+	Free->File = NULL;
+#endif
+
+#if MEM_DEBUG
+    memset (MemPtr, 0xDD, MemSize);
+#endif
+
+    /* Subtract chunk size from running total */
+ 
+    MAllocCurrent -= UserSize;
+    MAllocCurrentOverhead -= MEM_OVERHEAD_SIZE;
+
+
+    /* Free the memory */
+
+	free(MemPtr);
+
+}
+
+
+/* Mem_Realloc - realloc a block of memory */
+
+void *Mem_Realloc (void *Address, size_t Size, char *file, int line)
+{
+	void		*MemPtr;
+	MemHeader	*Header;
+	size_t		OldSize;
+
+	MReallocCalls++;
+
+	MemPtr =  Mem_Alloc(Size, file, line);
+
+	Header = (MemHeader *)Address - 1;
+	OldSize = Header->Size;
+
+	memcpy(MemPtr, Address, (OldSize < Size ? OldSize : Size));
+
+	Mem_Free(Address, file, line);
+
+//	printf("Realloc: %s line %d: Addr: %08X Size: %d to %d\n", file, line, Address, OldSize, Size);
+
+	return MemPtr;
+}
+
+
+/* Mem_Summary - Give a memory usage summary */
+
+void Mem_Summary(char *title, int final)
+{
+
+#if MEM_STATISTICS
+	printf("\nMemory usage summary: %s\n\n", title);
+	printf("Alloc calls: %d, Free calls: %d\n",  MAllocCalls,  MFreeCalls);
+	printf("Realloc calls: %d (included in above counts)\n",  MReallocCalls);
+	printf("Maximum memory usage: %d, Current usage: %d\n",  MAllocMaximum,  MAllocCurrent);
+#endif
+
+#if MEM_TRACE
+	if (final)
+	{
+		int	i;
+
+		printf("\nUnfreed memory: %s\n\n", title);
+		for (i = 0; i < MAX_TRACE; i++)
+			if (TraceData[i].File)
+				printf("Unfreed: %s line %d: Size: %d\n", 
+					TraceData[i].File, TraceData[i].Line, TraceData[i].Size);
+	}
+#endif
+
+}
+
+
+#endif
--- src/mem.h
+++ src/mem.h
@@ -1,6 +1,5 @@
 /*
-** Copyright (C) 1995, 1996, 1997, 1998 Hewlett-Packard Company
-** Originally by Kevin Hughes, kev@kevcom.com, 3/11/94
+$Id$
 **
 ** This program and library is free software; you can redistribute it and/or
 ** modify it under the terms of the GNU (Library) General Public License
@@ -16,12 +15,55 @@
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 **
+** Author: Bill Meier, June 2001
+**
 */
 
+/*
+** The following settings control the memory allocator. Each setting is independent.
+** They also affect the actual memory usage of the program, because (currently)
+** turning on any of these settings increases the size of each allocation.
+** MEM_STATISTICS allocates the least extra and MEM_DEBUG allocates the most extra per call. 
+**
+** In addition (currently) turning on any of these settings will map all
+** realloc calls into a alloc and free for simplier implementation. However, this
+** should be transparent to all programs!
+*/
+
+/*
+** Normal settings (but not required):
+**		If you turn on MEM_DEBUG, turn on MEM_STATISTICS
+**		If you turn on MEM_TRACE, turn on MEM_STATISTICS
+*/
+
+/* MEM_DEBUG checks for memory consistency on alloc/free */
+#define MEM_DEBUG 0
+
+/* MEM_TRACE checks for unfreed memory, and where it is allocated */
+#define MEM_TRACE 0
+
+/* MEM_STATISTICS gives memory statistics (bytes allocated, calls, etc */
+#define MEM_STATISTICS 0 
 
 
-void *emalloc (size_t n);
-void *erealloc (void *ptr, size_t n);
-void efree (void *ptr);
+/* The following are the basic malloc/realloc/free replacements */
+#if MEM_DEBUG | MEM_TRACE | MEM_STATISTICS
 
-void checkmem(void);
+#define emalloc(size) Mem_Alloc(size, __FILE__, __LINE__)
+#define erealloc(ptr, size) Mem_Realloc(ptr, size, __FILE__, __LINE__)
+#define efree(ptr) Mem_Free(ptr, __FILE__, __LINE__)
+
+void *Mem_Alloc(size_t size, char *file, int line);
+void *Mem_Realloc(void *ptr, size_t size, char *file, int line);
+void Mem_Free(void *ptr, char *file, int line);
+
+#else
+
+void *emalloc(size_t size);
+void *erealloc(void *ptr, size_t size);
+void efree(void *ptr);
+
+#endif
+
+/* Hook to print out statistics if enabled */
+void Mem_Summary(char *title, int final);
