7c8b425d6a86a56458f3f1d78231ef5cd4306030
jmruiz <jmruiz@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2001-07-06 17:11:45 +0000

    Berkeley DB initial module (db_berkeley_db.c).
    To use test it:
    
    1- Download db-3.2.9 from www.sleepycat.com
    2- Install it
    3- Modify routines initModule_DB and freeModule_DB in db.c. They must called
    initModule_DB_db(sw) and freeModule_DB_db(sw) to use db_berkeley_db.c
    instead of the db_native.c
    4- Add db_berkeley_db.o and -ldb  to the Makefile.
    5- PROPFILE must not be defined in config.h (Fix to do)
    6- Make and that's all
    
    
    NOTES:
    
    I do not know very well Berkeley DB. This is my first try with it. So, there
    can be much more ways to use it. This is just one and may be improved.
    
    Sendmail and other packages use db files. So be careful if you are using
    old versions of db if you install db-3.2.9.

---
+++ swish-e/src/db_berkeley_db.h
@@ -0,0 +1,102 @@
+/*
+**
+** This program and library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU (Library) General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU (Library) General Public License for more details.
+**
+** You should have received a copy of the GNU (Library) General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+**
+**
+**
+** 2001-07  jose ruiz  initial coding
+**
+*/
+
+
+
+#ifndef __HasSeenModule_DB_db
+#define __HasSeenModule_DB_db	1
+
+
+#include <db.h>
+
+struct Handle_DB_db
+{
+      /* word counter - Used to asign WordID's */
+   int wordcounter;
+      /* db file to store header */
+   DB *dbf_header;
+      /* db file to store words */
+   DB *dbf_words;
+      /* db file to store word's data */
+   DB *dbf_worddata;
+      /* db file to store word's inverted index */
+   DB *dbf_invertedindex;
+      /* db file to store docs's data */
+   DB *dbf_docs;
+      /* db file to store sorted indexes */
+   DB *dbf_sorted_indexes;
+      /* cursor to read header data */
+   DBC *cursor_header;
+      /* cursor to read the inverted word data */
+   DBC *cursor_inverted;
+};
+
+
+void initModule_DB_db (SWISH *);
+void freeModule_DB_db (SWISH *);
+int configModule_DB_db (SWISH *sw, StringList *sl);
+
+void   *DB_Create_db (char *dbname);
+void   *DB_Open_db (char *dbname);
+void    DB_Close_db(void *db);
+void    DB_Remove_db(void *db);
+
+int     DB_InitWriteHeader_db(void *db);
+int     DB_EndWriteHeader_db(void *db);
+int     DB_WriteHeaderData_db(int id, unsigned char *s, int len, void *db);
+
+int     DB_InitReadHeader_db(void *db);
+int     DB_ReadHeaderData_db(int *id, unsigned char **s, int *len, void *db);
+int     DB_EndReadHeader_db(void *db);
+
+int     DB_InitWriteWords_db(void *db);
+long    DB_GetWordID_db(void *db);
+int     DB_WriteWord_db(char *word, long wordID, void *db);
+int     DB_WriteWordHash_db(char *word, long wordID, void *db);
+long    DB_WriteWordData_db(long wordID, unsigned char *worddata, int lendata, void *db);
+int     DB_EndWriteWords_db(void *db);
+
+int     DB_InitReadWords_db(void *db);
+int     DB_ReadWordHash_db(char *word, long *wordID, void *db);
+int     DB_ReadFirstWordInvertedIndex_db(char *word, char **resultword, long *wordID, void *db);
+int     DB_ReadNextWordInvertedIndex_db(char *word, char **resultword, long *wordID, void *db);
+long    DB_ReadWordData_db(long wordID, unsigned char **worddata, int *lendata, void *db);
+int     DB_EndReadWords_db(void *db);
+
+
+int     DB_InitWriteFiles_db(void *db);
+int     DB_EndWriteFiles_db(void *db);
+int     DB_WriteFile_db(int filenum, unsigned char *filedata,int sz_filedata, void *db);
+int     DB_InitReadFiles_db(void *db);
+int     DB_ReadFile_db(int filenum, unsigned char **filedata,int *sz_filedata, void *db);
+int     DB_EndReadFiles_db(void *db);
+
+int     DB_InitWriteSortedIndex_db(void *db);
+int     DB_WriteSortedIndex_db(int propID, unsigned char *data, int sz_data,void *db);
+int     DB_EndWriteSortedIndex_db(void *db);
+ 
+int     DB_InitReadSortedIndex_db(void *db);
+int     DB_ReadSortedIndex_db(int propID, unsigned char **data, int *sz_data,void *db);
+int     DB_EndReadSortedIndex_db(void *db);
+
+
+#endif
