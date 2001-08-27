7ac660bfaafaeb279c6dc2b07e0d326388c65fd9
whmoseley <whmoseley@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2001-08-27 20:58:18 +0000

    Minor changes to docs.
    Also removed two filters that were duplicates and written before Rainer
    made it so filter programs could be called directly.

--- filter-bin/_binfilter.sh
+++ filter-bin/_binfilter.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Example of a shell script filter
+
 # Simple filter for binary files (eg: exe files) 
+
+# Note: This is just an example of a shell script.  In general you would not
+# use a shell script to just call a program -- rather call the program directly from
+# swish using a FileFilter command.
+# e.g. FileFilter .exe strings "'%p'"
 
 strings "$1" - 2>/dev/null
--- filter-bin/_doc2text.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# WinWord Filter 
-
-catdoc -s8859-1 -d8859-1 "$1" 2>/dev/null
