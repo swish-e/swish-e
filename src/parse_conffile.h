409ba213c31a1be17771bb1164808d79c0745b70
whmoseley <whmoseley@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2001-09-29 15:38:54 +0000

    A little doc work.
    Added a feature to select the parser with a header in -S prog which
    overrides IndexContents for that doc.
    
    Added the <meta name="robots" content="NOINDEX"> extenstion to the HTML2
    parser, to use the standard way to not index a document.

--- src/parse_conffile.h
+++ src/parse_conffile.h
@@ -8,12 +8,7 @@ $Id$
 void grabCmdOptions(StringList *sl, int start, struct swline **listOfWords);
 void getdefaults(SWISH *sw, char *conffile, int *hasdir, int *hasindex, int hasverbose);
 int getYesNoOrAbort (StringList *sl, int n, int islast);
-int getDocTypeOrAbort (StringList *sl, int n);
-
-void readstopwordsfile(SWISH *, IndexFILE *, char *);
-void readusewordsfile(SWISH *, IndexFILE *, char *);
-int parseconfline(SWISH *, StringList *);
-void readbuzzwordsfile(SWISH *, IndexFILE *, char *);
+int	strtoDocType( char * s );
 
 void free_Extracted_Path( SWISH *sw );
 void free_regex_list( regex_list **reg_list );
