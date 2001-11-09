42d4f3be25e2273f55c02548d5c30951eb1f4f6d
augur <augur@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2001-11-09 18:34:25 +0000

    Autoconf updates.  We are not currently using autoconf on Win32.  However, the autoconf model is an excellent one.  Using it's model we can probably eliminate all "ifdef _WIN32" statements and use our hand-made win32/config.h. This should even help porting to other systems which may be like Win32.

--- swish-e/src/win32/config.h
+++ swish-e/src/win32/config.h
@@ -6,21 +6,22 @@
 #  include "win32/regex.h"
 #endif
 
-#include <stdlib.h>			/* _sleep() */
-#include <process.h>		/* _getpid() */
-#include <io.h>				/* _umask(), _mktemp, etc */
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+/* Special Inclusions  */
+#include <stdlib.h>		/* _sleep() */
+#include <io.h>			/* _umask(), _mktemp, etc */
+#include <fcntl.h>		/* Most io.h functions want this */
+#include <sys/types.h>	/* Most io.h functions want this */
+#include <sys/stat.h>	/* Most io.h functions want this */
 
 /* ifdef logic  */
-#define NO_GETTOD			/* Win32 has no Get Time Of Day  */
-#define NO_SYMBOLIC_FILE_LINKS		/* Win32 has no symbolic links */
-#undef INDEXPERMS			/* Win32 has no chmod() */
+#define NO_GETTOD		/* Win32 has no Get Time Of Day  */
+#define NO_SYMBOLIC_FILE_LINKS	/* Win32 has no symbolic links */
+#undef INDEXPERMS		/* Win32 has no chmod() - DLN 2001-11-05 Umm, yes it does... */
+#define HAVE_STDLIB_H	/* We need stdlib.h instead of unistd.h  */
+#define HAVE_PROCESS_H  /* _getpid is here  */
 
 /* Macros which rewrite values  */
-#define SWISH_VERSION "2.1-dev-22"	/* Should we find a better way to handle this */
-#define TMPDIR "c:\\windows\\temp"	/* Should this be taken from the environment at runtime */
+#define SWISH_VERSION "2.1-dev-24"	/* Should we find a better way to handle this */
 #define DIRDELIMITER '\\'		/* Does this work right?  Might explain dir problems which have reported */
 
 /* Internal SWISH-E File Access Modes */
