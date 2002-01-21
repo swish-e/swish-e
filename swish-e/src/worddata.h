5b99cb60d7acbff09cc1275d817be5d636c80fdf
jmruiz <jmruiz@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2002-01-21 17:54:42 +0000

    The work for dynamic updating of the index database continues...

---
+++ swish-e/src/worddata.h
@@ -0,0 +1,29 @@
+/* */
+
+
+typedef struct WORDDATA_Page
+{
+    unsigned long      page_number;
+    int                used_blocks;
+    int                n;
+    int                modified;
+    int                in_use;
+
+    struct WORDDATA_Page  *next_cache;
+
+    unsigned char data[0];        /* Page data */
+} WORDDATA_Page;
+
+#define WORDDATA_CACHE_SIZE 97
+
+typedef struct WORDDATA
+{
+    WORDDATA_Page      *last_page;
+    struct WORDDATA_Page *cache[WORDDATA_CACHE_SIZE];
+    FILE *fp;
+} WORDDATA;
+
+WORDDATA *WORDDATA_Open(FILE *fp);
+void WORDDATA_Close(WORDDATA *bt);
+unsigned long WORDDATA_Put(WORDDATA *b, unsigned int len, unsigned char *data);
+unsigned char * WORDDATA_Get(WORDDATA *b, unsigned long global_id, unsigned int *len);
