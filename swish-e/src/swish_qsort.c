c5f96538b4eba533565b972e37a6925ed8ecf287
augur <augur@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2002-05-31 03:54:31 +0000

    Bug fix for redefinition of min(a, b) on Win32 or anywhere else it's already defined.
    I'm not sure why the min macro isn't defined.  Are we missing a header?
    Win32 defines min(a, b) and max(a, b) in stdlib.h under !_POSIX!, !__STDC__ !__cplusplus

--- swish-e/src/swish_qsort.c
+++ swish-e/src/swish_qsort.c
@@ -46,7 +46,9 @@ typedef int		 cmp_t (const void *, const void *);
 static inline char	*med3 (char *, char *, char *, cmp_t *);
 static inline void	 swapfunc (char *, char *, int, int);
 
+#ifndef min
 #define min(a, b)	(a) < (b) ? a : b
+#endif
 
 /*
  * Qsort routine from Bentley & McIlroy's "Engineering a Sort Function".
