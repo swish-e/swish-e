05d979dd7b9fbccc81c16ded6c75e9d88b36f7e0
whmoseley <whmoseley@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2002-09-17 06:52:18 +0000

    Little rewrite

--- filters/SWISH/Filters/ID3toHTML.pm
+++ filters/SWISH/Filters/ID3toHTML.pm
@@ -8,12 +8,40 @@ use HTML::Entities;
 # Define the sort order for this filter
 use vars qw/ %FilterInfo $VERSION /;
 
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 %FilterInfo = (
     type     => 2,  # normal filter 1-10
     priority => 50, # normal priority 1-100
 );
+
+# Convert known ID3v2 tags to metanames.
+
+my %id3v2_tags = (
+    TIT2 => 'song',            # 4.2.1 TIT2 Title/songname/content description
+    TYER => 'year',            # 4.2.1 TYER Year
+    TRCK => 'track',           # 4.2.1 TRCK Track number/Position in set 
+    TCOP => 'copyright',       # 4.2.1 TCOP Copyright message
+                               # * WinAMP seems to prepend a (C) to this value.
+
+    TPE1 => 'artist',          # 4.2.1 TPE1 Lead performer(s)/Soloist(s)
+    TALB => 'album',           # 4.2.1 TALB Album/Movie/Show title
+    TENC => 'encoded',         # 4.2.1 TENC Encoded by
+    TOPE => 'artist_original', # 4.2.1 TOPE Original artist(s)/performer(s)
+    TCOM => 'composer',        # 4.2.1 TCOM Composer
+    TCON => 'genre',           # 4.2.1 TCON Content type
+
+                                            # 4.3.2 WXXX User defined URL link frame
+    WXXX_URL => 'url',                      #  * URL => http://URL/HERE
+    WXXX_Description => 'url_description',  #  * Description => WinAMP provides no description
+
+                                       # 4.11 COMM Comments
+    COMM_Text => 'comment',            # * Text => COMMENT
+    COMM_Language => 'comment_lang',   # * Language => eng
+    COMM_short => 'comment_short'      # * short => WinAMP provides no short
+
+);
+
 
 sub filter {
     my $filter = shift;
@@ -24,7 +52,7 @@ sub filter {
     # We need a file name to pass to the conversion function
     my $file = $filter->fetch_filename;
 
-    my $content_ref = get_id3_content_ref( $file ) || return;
+    my $content_ref = get_id3_content_ref( $file );
     return unless $content_ref;
 
     # update the document's content type
@@ -40,116 +68,117 @@ sub get_id3_content_ref {
     my $mp3 = MP3::Tag->new($filename);
     
     # return unless we have a file with tags
-    return unless ref $mp3 && $mp3->get_tags();
+    return format_empty_doc( $filename )
+        unless ref $mp3 && $mp3->get_tags();
 
     # Here we will store all of the tag info
     my %metadata;
     # Convert tags to metadata giving ID3v2 precedence
-    %metadata = get_id3v1_tags($mp3, \%metadata);
-    %metadata = get_id3v2_tags($mp3, \%metadata);
+    get_id3v1_tags($mp3, \%metadata);
+    get_id3v2_tags($mp3, \%metadata);  # will replace any v1 tags that are the same
 
     # HTML or bust
     return %metadata
         ? format_as_html( \%metadata )
-        : format_empty_doc();
+        : format_empty_doc( $filename );
 }
     
 sub get_id3v1_tags {
     my ($mp3, $metadata) = @_;
+
+    return unless exists $mp3->{ID3v1};
+
     # Read all ID3v1 tags into metadata hash
-    if( exists $mp3->{ID3v1} ) {
-        my $id3v1 = $mp3->{ID3v1};
-        for ( qw/ artist album comment genre song track year / ) {
-            $metadata->{$_} = $id3v1->$_ if $id3v1->$_;
-        }
+    my $id3v1 = $mp3->{ID3v1};
+    for ( qw/ artist album comment genre song track year / ) {
+        $metadata->{$_} = $id3v1->$_ if $id3v1->$_;
     }
-    return %$metadata;
 }
 
 sub get_id3v2_tags {
     my ($mp3, $metadata) = @_;
 
     # Do we even have an ID3 v2 tag?
-    if( exists $mp3->{ID3v2} ){
-        # List of ID3v2 tags we wish to retrieve
-        my %id3v2_tags = (
-            # 4.2.1 TIT2 Title/songname/content description
-            TIT2 => 'song',
-            # 4.2.1 TYER Year
-            TYER => 'year',
-            # 4.2.1 TRCK Track number/Position in set 
-            TRCK => 'track',
-            # 4.2.1 TCOP Copyright message
-            # * WinAMP seems to prepend a (C) to this value.
-            TCOP => 'copyright',
-            # 4.2.1 TPE1 Lead performer(s)/Soloist(s)
-            TPE1 => 'artist',
-            # 4.2.1 TALB Album/Movie/Show title
-            TALB => 'album',
-            # 4.2.1 TENC Encoded by
-            TENC => 'encoded',
-            # 4.2.1 TOPE Original artist(s)/performer(s)
-            TOPE => 'artist_original',
-            # 4.2.1 TCOM Composer
-            TCOM => 'composer',
-            # 4.2.1 TCON Content type
-            TCON => 'genre',
-            # 4.3.2 WXXX User defined URL link frame
-            # * Description => WinAMP provides no description
-            # * URL => http://URL/HERE
-            WXXX_URL => 'url',
-            WXXX_Description => 'url_description',
-            # 4.11 COMM Comments
-            # * Text => COMMENT
-            # * Language => eng
-            # * short => WinAMP provides no short
-            COMM_Text => 'comment',
-            COMM_Language => 'comment_lang',
-            COMM_short => 'comment_short'
-        );
+    return unless exists $mp3->{ID3v2};
+
     
-        # Get the tag and a hash of frame ids.
-        my $id3v2 = $mp3->{ID3v2};
-        my $frameIDs_hash = $id3v2->get_frame_ids;
-        # Go through each frame and translate it to usable metadata
-        foreach my $frame (keys %$frameIDs_hash) {
-            my ($info, $name) = $id3v2->get_frame($frame);
-            # We have a user defined frame
-            if (ref $info) { 
-                # $$$ We really only want COMM and WXXX
-                while(my ($key,$val)=each %$info) {
-                    # Concatenate frame and key for our lookup hash
-                    $key = $id3v2_tags{${frame} . "_" .${key}}; 
-                    # Assign value if not empty and has a key
-                    $metadata->{$key} = encode_entities($val) if $val && $key;
-                }
-            }
-            # We have a simple frame
-            else { 
-                $metadata->{$id3v2_tags{$frame}} = encode_entities($info) unless !$info;
+    # Get the tag and a hash of frame ids.
+    my $id3v2 = $mp3->{ID3v2};
+
+    # keys are 4-character-codes and values are the long names
+    my $frameIDs_hash = $id3v2->get_frame_ids;
+
+
+    # Go through each frame and translate it to usable metadata
+    foreach my $frame (keys %$frameIDs_hash) {
+        my ($info, $name) = $id3v2->get_frame($frame);
+
+
+        # We have a user defined frame
+        if (ref $info) { 
+            # $$$ We really only want COMM and WXXX
+            while(my ($key,$val)=each %$info) {
+
+                next if $key =~ /^_/ || !$val;  # leading underscore means binary data
+
+                # Concatenate frame and key for our lookup hash
+                my $code = ${frame} . "_" .${key};
+
+                # fails when frame is appended with digits (e.g. "COMM01");
+                my $metaname = $id3v2_tags{$code} || $code;
+
+                # Assign value if not empty and has a key
+                $metadata->{$metaname} = encode_entities($val) if $val;
             }
         }
+        # We have a simple frame
+        else {
+            my $metaname = $id3v2_tags{$frame} || $frame || 'blank frame';
+            $metadata->{$metaname} = encode_entities($info) unless !$info;
+        }
     }
-    return %$metadata;
 }
     
 sub format_as_html {
     my $metadata = shift;
-    
-    my $return_value = "<html>\n<head>\n";
-    $return_value .= "<title>"  . $metadata->{song} . "</title>\n" if $metadata->{song};
-    $return_value .= "<meta name=\"" . $_ . "\" content=\"" . $metadata->{$_} . "\">\n" for (sort keys %$metadata);
-    $return_value .= "<body>\n";
-    $return_value .= "<p><a href=\"" . $metadata->{url} . "\">". ($metadata->{url_description} or $metadata->{url}) . "</a></p>\n" if $metadata->{url};
-    $return_value .= "<p name=\"comment\" lang=\"". (get_iso_lang($metadata->{comment_lang})) . "\">" . $metadata->{comment} . "</p>\n" if $metadata->{comment};
-    $return_value .= "</body>\n</html>\n";
-    
-    return $return_value;
+
+    my $title = $metadata->{song} || $metadata->{album} || $metadata->{artist} || 'No Title';
+    my $metas = join "\n", map { qq[<meta name="$_" content="$metadata->{$_}">] } sort keys %$metadata;
+
+    my $url = '';
+    if ( $metadata->{url} ) {
+        my $desc = $metadata->{url_description} || $metadata->{url};
+        $url = qq[<p><a href="$metadata->{url}">$desc</a>];
+    }
+
+    my $comment = '';
+    if ( $metadata->{comment} ) {
+        my $lang = get_iso_lang($metadata->{comment_lang} || 'en');  # wrong assuming "en"?
+        $comment = qq[<p name="comment" lang="$lang">$metadata->{comment}</p>];
+    }
+
+
+    return <<EOF;
+<html>
+ <head>
+   <title>$title</title>
+   $metas
+ </head>
+ <body>
+   $url
+   $comment
+  </body>
+</html>
+EOF
+
 }
 
 sub format_empty_doc {
-    # perhaps we might want to do something here
-    return;
+    my $filename = shift;
+    require File::Basename;
+    my $base = File::Basename::basename( $filename, '.mp3' );
+    
+    return format_as_html( { song => $base, notag => 1 } );
 }
 
 sub get_iso_lang {
