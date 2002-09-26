e0ec6b6d92fb97841d19b39c30f0068457969fd3
augur <augur@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2002-09-26 22:13:51 +0000

    Win32 Installer updates.

---
+++ swish-e/src/win32/vars.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+ECHO.       
+ECHO     Thank you for using SWISH-E 2.2! http://swish-e.org/
+ECHO     Please see COPYING.txt for redistribution terms.
+ECHO ------------------------------------------------------------------
+ECHO    This is vars.bat in your install directory.  
+ECHO    vars.bat executes when you run "SWISH-E Command Prompt".
+ECHO    You may add commands and environment variables to this file.
+ECHO.   
+
+REM Add any commands you wish executed in this file below this line
+REM They will be executed when you select the "SWISH-E Command Prompt" link.
+REM Or by manually executing vars.bat from your SWISH-E install directory.
+
+
+
