6b6a7ff6c8679c708cb860736ec8d8cccb065d9d
jmruiz <jmruiz@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2002-10-20 16:49:44 +0000

    Some cleanup  in some files (BTREE related files not being used).

--- swish-e/src/worddata.c
+++ swish-e/src/worddata.c
@@ -3,67 +3,11 @@
 #include <string.h>
 
 /*
-#define STANDALONE
 #define DEBUG
 */
 
-#ifdef STANDALONE
-
-/* Rourtines to make long integers portable */
-unsigned long PACKLONG(unsigned long num)
-{
-    unsigned long _i = 0L;
-    unsigned char *_s;
-
-    if (num)
-    {
-        _s = (unsigned char *) &_i;
-        _s[0] = (unsigned char) ((num >> 24) & 0xFF);
-        _s[1] = (unsigned char) ((num >> 16) & 0xFF);
-        _s[2] = (unsigned char) ((num >> 8) & 0xFF);
-        _s[3] = (unsigned char) (num & 0xFF);
-        return _i;
-    }
-    return num;
-}
-
-
-unsigned long UNPACKLONG(unsigned long num)
-{
-    unsigned char *_s = (unsigned char *) &num;
-
-    return (_s[0] << 24) + (_s[1] << 16) + (_s[2] << 8) + _s[3];
-}
-
-
-static int num = 0;
-char *emalloc(unsigned int size)
-{
-num++;
-return malloc(size);
-}
-
-void efree(void *p)
-{
-num--;
-free(p);
-}
-
-
-#else
-
 #include "mem.h"
-
-unsigned long PACKLONG(unsigned long num);
-unsigned long UNPACKLONG(unsigned long num);
-unsigned char *compress3(int num, unsigned char *buffer);
-int uncompress2(unsigned char **buffer);
-
-#endif  /* STANDALONE */
-
-
 #include "worddata.h"
-
 
 /* WORDDATA page size */
 /* !!!! Must no be less than 4096 and not greater than 65536*/
@@ -72,8 +16,6 @@ int uncompress2(unsigned char **buffer);
 #define WORDDATA_BlockSize (WORDDATA_PageSize >> 8)
 
 #define SizeInt32 4
-
-
 
 /* Round to WORDDATA_PageSize */
 #define WORDDATA_RoundPageSize(n) (((n) + WORDDATA_PageSize - 1) & (~(WORDDATA_PageSize - 1)))
