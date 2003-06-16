062b47c1ea21112a2757ca20fa66909a40dfc248
jmruiz <jmruiz@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2003-06-16 16:24:50 +0000

    Remove a couple of debug messages.

--- src/index.c
+++ src/index.c
@@ -2485,9 +2485,7 @@ int     indexstring(SWISH * sw, char *s, int filenum, int structure, int numMeta
                 case FUZZY_STEMMING_RU:
                 case FUZZY_STEMMING_FI:
 #endif
-printf("Antes Stemm index.c %p\n",indexf->header.fuzzy_data.fuzzy_routine);
                     stem_return = indexf->header.fuzzy_data.fuzzy_routine(&swishword, &lenswishword,indexf->header.fuzzy_data.fuzzy_args);
-printf("Despues Stemm index.c\n");
 
                     /* === 
                     if ( stem_return == STEM_NOT_ALPHA ) printf("Stem: not alpha in '%s'\n", swishword );
