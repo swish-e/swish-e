31f3e372da96dfdf6ec878db039c631e1c68d65d
whmoseley <whmoseley@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2004-10-02 23:53:10 +0000

    More spider updates.

--- prog-bin/SwishSpiderConfig.pl
+++ prog-bin/SwishSpiderConfig.pl
@@ -12,20 +12,13 @@ with the swish-e distribution.
 A spider.pl configuration file is not required as spider.pl has reasonable
 defaults.  In fact, it's recommended that you only use a spider.pl
 configuration file *after* successfully indexing with spider.pl's default
-settings.  To use the default settings set in your *swish-e* config file:
+settings. To use the default settings run the spider using the special magical
+word "default" as the first parameter:
 
-   SwishProgParameters default <url to spider>
-   IndexDir spider.pl
+    spider.pl default <URL> [...]
 
-The "default" parameter instructs spider.pl to use its
-default parameters.  To specify a spider configuration file use:
-
-   SwishProgParameters <path to config file>
-   IndexDir spider.pl
-
-If no parameters are passed to spider.pl (i.e. SwishProgParameters is not used)
-then spider.pl will look for a file called F<SwishSpiderConfig.pl> in the current
-directory.
+If no parameters are passed to spider.pl then spider.pl will look for a file
+called F<SwishSpiderConfig.pl> in the current directory.
 
 A spider.pl config file is useful when you need to change the default
 behavior of the way spider.pl operates.  For example, you may wish to index
@@ -46,26 +39,41 @@ Each server description is contained in a single Perl hash.
 
 For example, to index two sites define two Perl hashes:
 
-	my %main_site = (
-	    base_url   => 'http://example.com',
-	    same_hosts => 'www.example.com',
-	    email      => 'admin@example.com',
-	);
+        my %main_site = (
+            base_url   => 'http://example.com',
+            same_hosts => 'www.example.com',
+            email      => 'admin@example.com',
+        );
 
 
-	my %news_site = (
-	    base_url   => 'http://news.example.com',
-	    email      => 'admin@example.com',
-	);
+        my %news_site = (
+            base_url   => 'http://news.example.com',
+            email      => 'admin@example.com',
+        );
 
-	@servers = ( \%main_site, \%news_site );
+        @servers = ( \%main_site, \%news_site );
         1;
 
-	
 The above defines two Perl hashes (%main_site and %news_site) and then places
 a *reference* (the backslash before the name of the hash) to each of those
 hashes in the @servers array.  The "1;" at the end is required at the end
 of the file (Perl must see a true value at the end of the file).
+
+Let's start out with a simple example.  As of Swish-e 2.4.3 there's a new option
+that allow you to merge your config file with the default config file used when
+you specify "default" as the first parameter to F<spider.pl>.
+So, say you only wanted to change the limit the number of files
+indexed.
+
+    @servers = ( 
+        {
+            use_default_config => 1,  # same as using 'default'
+            max_files          => 100,
+        },
+    );
+    1;
+
+That last number one is important, by the way.  It keeps Perl happy.
 
 Below are two example configurations, but included in the same @servers
 array (as anonymous Perl hashes).  They both have the skip flag set which
@@ -90,14 +98,13 @@ this with the "perldoc" command you will not see the examples below.
 
   ### Please do not spider these examples -- spider your own servers, with permission ####
 
-@servers = (
 
     #=============================================================================
     # This is a simple example, that includes a few limits
     # Only files ending in .html will be spidered (probably a bit too restrictive)
-    {
+    @ servers = ({
         skip        => 1,  # skip spidering this server
-        
+
         base_url    => 'http://www.swish-e.org/index.html',
         same_hosts  => [ qw/swish-e.org/ ],
         agent       => 'swish-e spider http://swish-e.org/',
@@ -111,7 +118,8 @@ this with the "perldoc" command you will not see the examples below.
         max_files   => 100,       # Max Unique URLs to spider
         max_indexed => 20,        # Max number of files to send to swish for indexing
         keep_alive  => 1,         # enable keep alives requests
-    },
+    } );
+    1;
 
     #=============================================================================
     # This example just shows more settings, and makes use of the SWISH::Filter
@@ -122,7 +130,12 @@ this with the "perldoc" command you will not see the examples below.
     # This example also only indexes the "docs" sub-tree of the swish-e
     # site by checking the path of the URLs
 
-    {
+    # Let spider.pl setup SWISH::Filter
+    # These will be used in the config below
+
+    my ($filter_sub, $response_sub) = swish_filter();
+
+    @servers = ({
         skip        => 0,         # Flag to disable spidering this host.
 
         base_url    => 'http://swish-e.org/current/docs/',
@@ -132,7 +145,7 @@ this with the "perldoc" command you will not see the examples below.
         keep_alive  => 1,         # Try to keep the connection open
         max_time    => 10,        # Max time to spider in minutes
         max_files   => 20,        # Max files to spider
-	delay_secs  => 2,         # Delay in seconds between requests
+        delay_secs  => 2,         # Delay in seconds between requests
         ignore_robots_file => 0,  # Don't set that to one, unless you are sure.
 
         use_cookies => 1,         # True will keep cookie jar
@@ -146,23 +159,20 @@ this with the "perldoc" command you will not see the examples below.
                                   # content.  Will trap / and /index.html,
                                   # for example.
 
-	# This will generate A LOT of debugging information to STDOUT
+        # This will generate A LOT of debugging information to STDOUT
         debug       => DEBUG_URL | DEBUG_SKIPPED | DEBUG_HEADERS,
 
 
         # Here are hooks to callback routines to validate urls and responses
         # Probably a good idea to use them so you don't try to index
         # Binary data.  Look at content-type headers!
-        
- #       test_url        => \&test_url,
- #       test_response   => \&test_response,
-        filter_content  => \&filter_content,        
-    },
 
+        test_url        => \&test_url,
+        test_response   => $response_sub,
+        filter_content  => $filter_sub,
+    } );
+    1;
 
-
-);    
-    
 
 #---------------------- Public Functions ------------------------------
 #  Here are some examples of callback functions
@@ -190,11 +200,14 @@ sub test_url {
 
     # make sure that the path is limited to the docs path
     return $uri->path =~ m[^/current/docs/];
-    
 }
 
 
-
+## Here's an example of a "test_response" callback.  You would
+# add it to your config like:
+#
+#   test_response => \&test_response_sub,
+#
 # This routine is called when the *first* block of data comes back
 # from the server.  If you return false no more content will be read
 # from the server.  $response is a HTTP::Response object.
@@ -204,7 +217,7 @@ sub test_url {
 # do not want to index.  But we also have a lot of image files that we want
 # to index the path name only.
 
-sub test_response {
+sub test_response_sub {
     my ( $uri, $server, $response ) = @_;
 
     return if $response->content_type =~ m[^audio/];
@@ -216,63 +229,6 @@ sub test_response {
 
 
 
-# This is an example of how to use the SWISH::Filter module included
-# with the swish-e distribution.
-#
-# Returns:
-#      true if content-type is text/* or if the document was filtered
-#      false if document was not filtered
-#      aborts if module cannot be loaded or filter object cannot be created.
-#
-
-my $filter;  # cache the object.
-
-sub filter_content {
-    my ( $uri, $server, $response, $content_ref ) = @_;
-
-    # Uncomment this to enable debugging of SWISH::Filter
-    # $ENV{FILTER_DEBUG} = 1;
-
-    my $content_type = $response->content_type;
-
-    # Ignore text/* content type -- no need to filter
-    return 1 if !$content_type || $content_type =~ m!^text/!;
-    
-
-    # Load the module - returns FALSE if cannot load module.
-    unless ( $filter ) {
-        eval { require SWISH::Filter };
-        if ( $@ ) {
-            $server->{abort} = $@;
-            return;
-        }
-        $filter = SWISH::Filter->new;
-        unless ( $filter ) {
-            $server->{abort} = "Failed to create filter object";
-            return;
-        }
-    }
-
-    # If not filtered return false and doc will be ignored (not indexed)
-    
-    my $doc = $filter->convert(
-        document => $content_ref,
-        name     => $response->base,
-        content_type => $content_type,
-    );
-    return unless $doc;
-    # return unless $doc->was_filtered # could do this since checking for text/* above
-    return if $doc->is_binary;
-
-    $$content_ref = ${$doc->fetch_doc};
-
-    # let's see if we can set the parser.
-    $server->{parser_type} = $doc->swish_parser_type || '';
-
-    return 1;
-}
-
-## Must return a true value!!
-
+# Dont' forget to return a true value at the end...
 1;
 
