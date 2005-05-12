39762b346066bfc4f5da5f2db3eca13b42b52bed
karman <karman@6fbdcf2a-5c28-0410-939a-9aac04d3518e>
2005-05-12 15:41:22 +0000

    changed license header to refer to COPYING for exception details

--- swish-e/src/modules.h
+++ swish-e/src/modules.h
@@ -19,30 +19,9 @@ $Id$
     along  with Swish-e; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
     
-    Swish-e includes a library for searching with a well-defined API. The library
-    is named libswish-e.
-    
-    Linking libswish-e statically or dynamically with other modules is making a
-    combined work based on Swish-e.  Thus, the terms and conditions of the GNU
-    General Public License cover the whole combination.
-
-    As a special exception, the copyright holders of Swish-e give you
-    permission to link Swish-e with independent modules that communicate with
-    Swish-e solely through the libswish-e API interface, regardless of the license
-    terms of these independent modules, and to copy and distribute the
-    resulting combined work under terms of your choice, provided that
-    every copy of the combined work is accompanied by a complete copy of
-    the source code of Swish-e (the version of Swish-e used to produce the
-    combined work), being distributed under the terms of the GNU General
-    Public License plus this exception.  An independent module is a module
-    which is not derived from or based on Swish-e.
-
-    Note that people who make modified versions of Swish-e are not obligated
-    to grant this special exception for their modified versions; it is
-    their choice whether to do so.  The GNU General Public License gives
-    permission to release a modified version without this exception; this
-    exception also makes it possible to release a modified version which
-    carries forward this exception.
+    See the COPYING file that accompanies the Swish-e distribution for details
+    of the GNU GPL and the special exception available for linking against
+    the Swish-e library.
     
 ** Mon May  9 18:19:34 CDT 2005
 ** added GPL
